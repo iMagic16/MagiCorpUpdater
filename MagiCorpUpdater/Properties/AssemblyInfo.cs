4a2ef9caf3c06824cc984fb2639087fb04b69e8d
Magic <jameswalsh5683@gmail.com>
2015-02-14 03:54:18 +0000

    rename

--- MagiCorpUpdater/Properties/AssemblyInfo.cs
+++ MagiCorpUpdater/Properties/AssemblyInfo.cs
@@ -1,12 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MagiCorp Updater")]
-[assembly: AssemblyDescription("Modular program updater for MagiCorp programs.")]
+[assembly: AssemblyTitle("MUP")]
+[assembly: AssemblyDescription("Modular update program")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("MagiCorp")]
 [assembly: AssemblyProduct("MagiCorp Updater")]
@@ -34,3 +35,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-GB")]
