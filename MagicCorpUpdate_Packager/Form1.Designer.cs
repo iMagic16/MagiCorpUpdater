046f53509e24274c02e8afc1ac0c2e58cb79f291
James W <jameswalsh5683@gmail.com>
2015-03-09 12:16:39 +0000

    Started Packager Program

---
+++ MagicCorpUpdate_Packager/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace MagicCorpUpdate_Packager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.LblValid = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtPasswd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtHostname = new System.Windows.Forms.TextBox();
+            this.LblHostname = new System.Windows.Forms.Label();
+            this.BtnSubmit = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(407, 130);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(75, 20);
+            this.BtnBrowse.TabIndex = 0;
+            this.BtnBrowse.Text = "Browse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File to upload: ";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.TxtHostname);
+            this.groupBox1.Controls.Add(this.LblHostname);
+            this.groupBox1.Controls.Add(this.TxtUsername);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.TxtPasswd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.LblValid);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.BtnBrowse);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 63);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(562, 262);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Packager";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(313, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // LblValid
+            // 
+            this.LblValid.AutoSize = true;
+            this.LblValid.Location = new System.Drawing.Point(489, 134);
+            this.LblValid.Name = "LblValid";
+            this.LblValid.Size = new System.Drawing.Size(63, 13);
+            this.LblValid.TabIndex = 3;
+            this.LblValid.Text = "VAL/INVAL";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 104);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(313, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Program Name:";
+            // 
+            // TxtPasswd
+            // 
+            this.TxtPasswd.Location = new System.Drawing.Point(88, 78);
+            this.TxtPasswd.Name = "TxtPasswd";
+            this.TxtPasswd.Size = new System.Drawing.Size(313, 20);
+            this.TxtPasswd.TabIndex = 7;
+            this.TxtPasswd.UseSystemPasswordChar = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Password: ";
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Location = new System.Drawing.Point(88, 52);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(313, 20);
+            this.TxtUsername.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 56);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Username: ";
+            // 
+            // TxtHostname
+            // 
+            this.TxtHostname.Location = new System.Drawing.Point(88, 26);
+            this.TxtHostname.Name = "TxtHostname";
+            this.TxtHostname.Size = new System.Drawing.Size(313, 20);
+            this.TxtHostname.TabIndex = 11;
+            // 
+            // LblHostname
+            // 
+            this.LblHostname.AutoSize = true;
+            this.LblHostname.Location = new System.Drawing.Point(6, 30);
+            this.LblHostname.Name = "LblHostname";
+            this.LblHostname.Size = new System.Drawing.Size(58, 13);
+            this.LblHostname.TabIndex = 10;
+            this.LblHostname.Text = "Hostname:";
+            // 
+            // BtnSubmit
+            // 
+            this.BtnSubmit.Location = new System.Drawing.Point(12, 331);
+            this.BtnSubmit.Name = "BtnSubmit";
+            this.BtnSubmit.Size = new System.Drawing.Size(562, 54);
+            this.BtnSubmit.TabIndex = 12;
+            this.BtnSubmit.Text = "Submit";
+            this.BtnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 397);
+            this.Controls.Add(this.BtnSubmit);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MUPdate Packager";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox TxtHostname;
+        private System.Windows.Forms.Label LblHostname;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtPasswd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LblValid;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BtnSubmit;
+    }
+}
+
